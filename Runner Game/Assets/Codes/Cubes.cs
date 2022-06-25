a9972656018bd07eed4ee6d2f05aa20386a7dc4e
Talha Enes İSLAMOĞLU <teislamoglu96@gmail.com>
2022-06-25 18:50:16 +0300

    add CreateCube

--- Runner Game/Assets/Codes/Cubes.cs
+++ Runner Game/Assets/Codes/Cubes.cs
@@ -1,14 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Cubes : MonoBehaviour
 {
+    [SerializeField] GameObject cubePart;
     GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.Find("Player");
+        CreateCube();
+    }
+
+    private void CreateCube()
+    {
+        for (float i = -0.5f; i < 0.5f; i = i + 0.1f)
+        {
+            //Instantiate(cubePart,new Vector3())
+        }
     }
 
     // Update is called once per frame
